56034d0c50d3309787497530523167a21595fad6
Jr Domingos <domingoshot@hotmail.com>
2020-05-10 12:21:46 -0300

    Update ServiceOrderApiApplication.java

--- src/main/java/com/jrd/so/ServiceOrderApiApplication.java
+++ src/main/java/com/jrd/so/ServiceOrderApiApplication.java
@@ -1,5 +1,7 @@
 package com.jrd.so;
 
+                                     /* ***********  @Author : JrD - Junior Domingos *********** */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
